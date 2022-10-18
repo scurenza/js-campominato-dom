847303913c988689108e2c0bc3cea3500ed5a9ec
scurenza <fedechiar@hotmail.it>
2022-10-18 12:38:07 +0200

    problemi variabile

--- js/script.js
+++ js/script.js
@@ -77,6 +77,48 @@ function createSquares (innerNum) {
 
 function actionOnSquares () {
     const squareNum = parseInt(this.textContent);
+    if (arrayBomb.includes(squareNum)) {
+        this.classList.add("red");
+        console.log("Bomba num:",squareNum);
+    } else {
     this.classList.add("blue");
     console.log("Cella num:",squareNum);
+    }
 }
+
+
+
+// SECONDA CONSEGNA
+/* **Consegna**
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+****
+Generare una griglia di gioco quadrata in cui ogni cella contiene un numero compreso tra 1 e 100.
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+**BONUS:**
+1 - L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+**2- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+****3- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste */
+
+
+const arrayBomb = rndBomb(49);
+console.log(arrayBomb);                
+
+// Funzione per creare le bombe
+function rndBomb (maxSquares) {
+    let bombs = [];
+    while (bombs.length < 16) {
+        const rndBomb = Math.floor(Math.random() * (maxSquares - 1 + 1) ) + 1;
+        if (!bombs.includes(rndBomb)) {
+            bombs.push(rndBomb);
+        }
+    }
+    return bombs;
+}
+
